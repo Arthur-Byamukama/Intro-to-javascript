465e4682319d775ce7f7a208927e388f9c76b38c
Arthur-Byamukama <arthur.byamukama30@gmail.com>
2019-03-14 12:27:01 +0300

    billing.js

--- billing.js
+++ billing.js
@@ -2,4 +2,4 @@
 const transactionCharge=0.025;
 var amountTendered=100000;
 console.log('This is what the billed amount is (less transaction charges);');
-console.log(amountTendered-transactionCharge*amountTendered);
+console.log(amountTendered-transactionCharge*amountTenderd);
